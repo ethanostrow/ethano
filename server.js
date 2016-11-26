b8aa4a3435fa50b276a5454d676ae19885f1b359
ethanostrow <codethanos@gmail.com>
2016-11-26 00:13:24 -0800

    please work
    i spelled gitignore wrong last time

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+
+// BASE SETUP
+// ==============================================
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+
+// home page route (http://localhost:3000)
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+// apply the routes to our application
+app.use('/', router);
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
