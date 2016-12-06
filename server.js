558ffcb3e3b086f6c9f828d94143c1b25ee5ccf2
ethanostrow <codethanos@gmail.com>
2016-12-05 23:28:26 -0800

    lets try sendgrid
    sendgrid!

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
 
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+var helper = require('sendgrid').mail
+
+subject = "Sending with SendGrid is Fun"
+content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js")
+mail = new helper.Mail(from_email, subject, to_email, content)
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON()
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode)
+  console.log(response.body)
+  console.log(response.headers)
+})
+
 // home page route (http://localhost:3000)
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
